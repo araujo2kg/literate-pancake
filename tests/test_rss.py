8f21c5bb8b3fb673a478e8e3e8017e8a2d09000d
araujo2kg <araujo2kg@gmail.com>
2024-09-04 20:21:36 -0300

    Update rss test.

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -1,4 +1,9 @@
-def test_rss(client):
+def test_rss(client, auth):
+    # Create post without image to assert default image is inserted
+    auth.login()
+    client.post(
+        "/create", data={"title": "no image", "body": "testing", "tags": ""}
+    )
     response = client.get("/rss")
     assert response.status_code == 200
     assert response.content_type == "application/rss+xml"
@@ -6,3 +11,4 @@ def test_rss(client):
     assert b"<rss" in response.data
     assert b"<title>Flaskr</title>" in response.data
     assert b"<title>test title</title>" in response.data
+    assert b"<title>no image</title>" in response.data
